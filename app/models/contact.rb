b78c7adf67e419df7159d7cddac91747aeb229b6
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2018-01-21 18:32:32 +0200

    add phones validations and asscoiation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ApplicationRecord
+  has_many :phones
+  validates :name, presence: true
+  validates :phones, presence: true
+
+  accepts_nested_attributes_for :phones, allow_destroy: true
 end
