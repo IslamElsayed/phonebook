59f197b6cd496403874db93a473ba8e1e9b9623e
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2018-01-21 22:52:49 +0200

    add name uniqueness validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ApplicationRecord
   has_many :phones
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :phones, presence: true
 
   accepts_nested_attributes_for :phones, allow_destroy: true
