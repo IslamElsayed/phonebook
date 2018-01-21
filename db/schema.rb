5735b2d7384129aae491eb5686988fd8ded164e0
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2018-01-21 18:33:40 +0200

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180120145516) do
+ActiveRecord::Schema.define(version: 20180121150321) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
-    t.string "phones"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "phones", force: :cascade do |t|
+    t.string "number"
+    t.integer "contact_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contact_id"], name: "index_phones_on_contact_id"
   end
 
 end
